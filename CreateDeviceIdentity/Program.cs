99e75d8aef006f600ebec91d506a050e7ca2230c
PickwickH <wilfried.heermann@outlook.com>
2017-05-11 21:33:51 +0200

    Files added

---
+++ CreateDeviceIdentity/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateDeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=whpickwick.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=qi2W6PghBWqVMBiHegYSQBlYCrvFzVCOWF+KiKj9Yos=";
+        
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "myFirstDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+            /// <summary>
+            /// Console.WriteLine("Generated device key Copy: {0}", registryManager.AddDeviceAsync(new Device(deviceId));
+            /// <returns></returns>
+        }
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ SimulatedDevice/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static string iotHubUri = "whpickwick.azure-devices.net";
+        static string deviceKey = "PTV85oVK6/wBt/K/X4tWeuN7KOvnpeLAiz284kkyvjc=";
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double minTemperature = 20;
+            double minHumidity = 60;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "myFirstDevice",
+                    temperature = currentTemperature,
+                    humidity = currentHumidity
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                await Task.Delay(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myFirstDevice", deviceKey), TransportType.Mqtt);
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+    }
+}
